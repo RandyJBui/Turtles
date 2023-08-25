8e63417b409471a6c1e7e5c00d52dff9d0c70732
RandyJBui <90497918+RandyJBui@users.noreply.github.com>
2023-08-24 22:03:26 -0700

    Update dig.lua

--- dig.lua
+++ dig.lua
@@ -15,65 +15,65 @@ function needFuel()
 end
 function goX(x,y,z)
     local x1,y1,z1 = gps.locate()
-    turtle.forward()
-    local x2,y2,z2 = gps.locate()
-    turtle.back()
-    while(x2 == x1) do
-        turtle.turnLeft( )
+    if x ~= x1 then
         turtle.forward()
-        x2,y2,z2 = gps.locate()
+        local x2,y2,z2 = gps.locate()
+        turtle.back()
+
+        while(x2 == x1) do
+            turtle.turnLeft( )
+            turtle.forward()
+            x2,y2,z2 = gps.locate()
+        end
+        if( math.abs(x - x1) < math.abs(x - x2)) then
+            turtle.turnLeft()
+            turtle.turnLeft()
+        end 
     end
-    if( math.abs(x - x1) < math.abs(x - x2)) then
-        turtle.turnLeft()
-        turtle.turnLeft()
-    end 
 end
 function goZ(x,y,z)
     local x1,y1,z1 = gps.locate()
-    turtle.forward()
-    local x2,y2,z2 = gps.locate()
-    turtle.back()
-    while(z2 == z1) do
-        turtle.turnLeft( )
+    if z ~= z1 then
         turtle.forward()
-        x2,y2,z2 = gps.locate()
+        local x2,y2,z2 = gps.locate()
+        turtle.back()
+        while(z2 == z1) do
+            turtle.turnLeft( )
+            turtle.forward()
+            x2,y2,z2 = gps.locate()
+        end
+        if( math.abs(z - z1) < math.abs(z - z2)) then
+            turtle.turnLeft()
+            turtle.turnLeft()
+        end 
     end
-    if( math.abs(z - z1) < math.abs(z - z2)) then
-        turtle.turnLeft()
-        turtle.turnLeft()
-    end 
 end
 function goToStart(x,y,z)
    local x1,y1,z1 = gps.locate()
-   goX(x,y,z)
-    while(x ~= x1) do
-        turtle.forward()
-        x1,y1,z1 = gps.locate()
-    end
-    while(y ~= y1) do
-        if(y - y1 > 0) then
-            turtle.up()
+   if x1 == x and y1 == y and z1 == z then
+        print("Here")
+   else
+        goX(x,y,z)
+        while(x ~= x1) do
+            turtle.forward()
+            x1,y1,z1 = gps.locate()
         end
-        if(y - y1 < 0) then
-            turtle.up()
+        while(y ~= y1) do
+            if(y - y1 > 0) then
+                turtle.up()
+            end
+            if(y - y1 < 0) then
+                turtle.up()
+            end
+            x1,y1,z1 = gps.locate()
         end
-        x1,y1,z1 = gps.locate()
+        goZ(x,y,z)
+        while(z ~= z1) do
+            turtle.forward()
+            x1,y1,z1 = gps.locate()
+        end
     end
-    goZ(x,y,z)
-    while(z ~= z1) do
-        turtle.forward()
-        x1,y1,z1 = gps.locate()
-    end
-
 end
 
 local x,y,z = gps.locate()
-for i =0,5,1 do
-turtle.forward()
-end
-turtle.turnLeft()
-for i =0,3,1 do
-    turtle.forward()
-end
-
 goToStart(x,y,z)
